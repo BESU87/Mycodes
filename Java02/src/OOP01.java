e4cfb1a45312a20b79fb1b58b6daaaa349b99b77
BESU87 <117749636+BESU87@users.noreply.github.com>
2024-11-05 19:03:48 +0300

    Update OOP01.java
    from browser

--- Java02/src/OOP01.java
+++ Java02/src/OOP01.java
@@ -1,6 +1,6 @@
 public class OOP01 {
     public static void main(String[] args) {
         System.out.println("Testing OOP2");
-        System.out.println(" Testing OOP3");
+        System.out.println(" Testing OOP3334");
     }
 }
